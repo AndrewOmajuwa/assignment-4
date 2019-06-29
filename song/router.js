4681e217b4b48b5dd9298f96ca840a25017594cf
AndrewOmajuwa <omajuwaandrew@gmail.com>
2019-06-29 18:59:20 +0200

    clean up

--- song/router.js
+++ song/router.js
@@ -1,20 +1,18 @@
 //import router from express
-const { Router } = require('express')
+const { Router } = require("express");
 
 //import song model
-const Song = require('./model')
+const Song = require("./model");
 
 //instantiate router
-const router = new Router()
+const router = new Router();
 
 //define endpoint to post song to playlist
 
-router.post('/playlists/:id/song', (req, res, next) => {
-    Song.create(req.body)
+router.post("/playlists/:id/songs", (req, res, next) => {
+  Song.create(req.body)
     .then(song => res.status(201).json(song))
-    .catch(error => next(error))
-})
+    .catch(error => next(error));
+});
 
-
-
-module.exports = router
+module.exports = router;
